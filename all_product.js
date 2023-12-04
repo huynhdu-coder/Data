92340b181f9537315d8e3d8f38ff0f69e5ced2a1
huynhdu-coder <114698130+huynhdu-coder@users.noreply.github.com>
2023-12-04 13:34:44 -0500

    Add files via upload

---
+++ all_product.js
@@ -0,0 +1,383 @@
+import womenProduct1 from './women/women_pro_1.webp'
+import womenProduct2 from './women/women_pro_2.webp'
+import womenProduct3 from './women/women_pro_3.webp'
+import womenProduct4 from './women/women_pro_4.webp'
+import womenProduct5 from './women/women_pro_5.webp'
+import womenProduct6 from './women/women_pro_6.webp'
+import womenProduct7 from './women/women_pro_7.webp'
+import womenProduct8 from './women/women_pro_8.webp'
+import womenProduct9 from './women/women_pro_9.webp'
+import womenProduct10 from './women/women_pro_10.webp'
+import womenProduct11 from './women/women_pro_11.webp'
+import womenProduct12 from './women/women_pro_12.webp'
+import womenProduct13 from './women/women_pro_13.webp'
+
+import homeProduct1 from './home/home_pro_1.webp'
+import homeProduct2 from './home/home_pro_2.webp'
+import homeProduct3 from './home/home_pro_3.webp'
+import homeProduct4 from './home/home_pro_4.webp'
+import homeProduct5 from './home/home_pro_5.webp'
+import homeProduct6 from './home/home_pro_6.webp'
+import homeProduct7 from './home/home_pro_7.webp'
+import homeProduct8 from './home/home_pro_8.webp'
+
+import menProduct1 from './men/men_pro_1.webp'
+import menProduct2 from './men/men_pro_2.webp'
+import menProduct3 from './men/men_pro_3.webp'
+import menProduct4 from './men/men_pro_4.webp'
+import menProduct5 from './men/men_pro_5.webp'
+import menProduct6 from './men/men_pro_6.webp'
+import menProduct7 from './men/men_pro_7.webp'
+import menProduct8 from './men/men_pro_8.webp'
+import menProduct9 from './men/men_pro_9.webp'
+
+import accProduct1 from "./shoe/acc_pro_1.webp"
+
+import shoe1 from './shoe/shoe_1.webp'
+import shoe2 from './shoe/shoe_2.webp'
+import shoe3 from './shoe/shoe_3.webp'
+import shoe4 from './shoe/shoe_4.webp'
+import shoe5 from './shoe/shoe_5.webp'
+import shoe6 from './shoe/shoe_6.webp'
+import shoe7 from './shoe/shoe_7.webp'
+import shoe8 from './shoe/shoe_8.webp'
+
+let all_product = [
+    {
+        id: 1,
+        productName: "Rest Day Sweats Cropped Pullover",
+        category:"womens",
+        productColor: "Evening Blue",
+        price: 64,
+        productImage: womenProduct1,
+
+    },   {
+        id: 2,
+        productName: "Adapt Fleck Seamless Leggings",
+        category:"womens",
+        productColor: "Evening Blue",
+        price: 38,
+        productImage: womenProduct2,
+
+    },
+    {
+        id: 3,
+        productName: "Rest Day Sweats Hoodie",
+        category:"womens",
+        productColor: "Black Marl",
+        price: 45,
+        productImage: womenProduct3,
+
+    },
+    {
+        id: 4,
+        productName: "Adapt Fleck Seamless Leggings",
+        price: 38,
+        category:"womens",
+        productColor: "Black",
+        productImage: womenProduct4,
+
+    },
+    {
+        id: 5,
+        productName: "Training Oversized Fleece Hoodie",
+        price: 45.0,
+        category:"womens",
+        productColor: "Black",
+        productImage: womenProduct5,
+
+    },
+    {
+        id: 6,
+        productName: "Everyday Seamless Leggings",
+        price: 38,
+        category:"womens",
+        productColor: "Purple",
+        productImage:womenProduct6,
+
+    },
+    {
+        id: 7,
+        productName: "Racerback Sports Bra",
+        price: 24,
+        category:"womens",
+        productColor: "Black",
+        productImage: womenProduct7,
+
+    },
+    {
+        id: 8,
+        productName: "Training Shocks",
+        price: 12,
+        category:"accessories",
+        productColor: "White",
+        productImage: womenProduct8,
+
+    },
+    {
+        id: 9,
+        productName: "Strong Peach Crop Tank",
+        price: 30,
+        category:"womens",
+        productColor: "Candy Floss Pink",
+        productImage: womenProduct9,
+
+    },
+    {
+        id: 10,
+        productName: "Heavyweight Loopback Sweat Pullover",
+        price: 70,
+        category:"womens",
+        productColor: "Asphalt Grey",
+        productImage: womenProduct10,
+
+    },
+    {
+        id: 11,
+        productName: "Thermal Fleece 1/4 Zip Pullover",
+        price: 60,
+        category:"womens",
+        productColor: "Candy Floss Pink",
+        productImage: womenProduct11,
+
+    },
+    ////////////////home/////////////
+
+    {
+        id: 101,
+        productName: "Block Oversized T-Shirt",
+        category: "womens",
+        category: "topSeller",productColor:"Rust Orange",
+
+        price: 30,
+        productImage: homeProduct1,
+        
+    },
+    {
+        id: 102,
+        productName: "Legacy Joggers",
+        category: "womens",
+        productColor:"Nutmeg Orange",
+        price: 60,
+        productImage: homeProduct2,
+
+    },
+    {
+        id: 103,
+        productName: "Arrival Marl T-Shirt",
+        category: "mens",
+        productColor:"Ignite Orange",
+        price: 22,
+        productImage: homeProduct3,
+
+    },
+    {
+        id: 104,
+        productName: "Arrivals 5'' Shorts",
+        category: "mens",
+        productColor:"Ignite Orange",
+        price: 26,
+        productImage: homeProduct4,
+
+    },
+    {
+        id: 105,
+        productName: "Legacy Wahsed Crop Top",
+        price: 46,
+        category: "womens",
+        productColor:"8 Color",
+        productImage: homeProduct5,
+
+    },
+    {
+        id: 106,
+        productName: "Apex Run 4'' Shorts",
+        price: 38,
+        category: "mens",
+        productColor:"Charred Orange",
+        productImage: homeProduct6,
+
+    },
+    {
+        id: 107,
+        productName: "Cozzy Beanie",
+        price: 110,
+        category: "accessories",
+        productColor:"Rust Orange",
+        productImage: homeProduct7,
+
+    },
+    {
+        id: 108,
+        productName: "Nike Calm",
+        price: 50,
+        category: "accessories",
+        productColor:"Pure White",
+        productImage: homeProduct8,
+
+    },
+////////////////////men////////////////
+{
+    id: 201,
+    productName: "GS X Francis Graphic Tee",
+    price: 50,
+    category:"mens",
+    productColor: "Black",
+    productImage: menProduct1,
+
+},
+{
+    id: 202,
+    productName: "Lifting Club Mesh 5'' Shorts",
+    price: 40,
+    category:"mens",
+    productColor: "Off White",
+    productImage: menProduct2,
+
+},
+{
+    id: 203,
+    productName: "Essential Oversized Zip Up Hoddie",
+    price: 56,
+    category:"mens",
+    productColor: "Charcoal Grey Marl",
+    productImage: menProduct3,
+
+},
+{
+    id: 204,
+    productName: "Lifting Club Long Sleeve T-Shirt ",
+    price: 32,
+    category:"mens",
+    productColor: "White",
+    productImage: menProduct4,
+
+},
+{
+    id: 205,
+    productName: "Retro Track Pants",
+    price: 70,
+    category:"mens",
+    productColor: "Black",
+    productImage: menProduct5,
+
+},
+{
+    id: 206,
+    productName: "Retro Track Jacket",
+    price: 76,
+    category:"mens",
+    productColor: "Black",
+    productImage: menProduct6,
+
+},
+{
+    id: 207,
+    productName: "Arrival 7'' Shorts",
+    price: 26,
+    category:"mens",
+    productColor: "Stone Grey",
+    productImage: menProduct7,
+
+},
+{
+    id: 208,
+    productName: "Sport Seamless Tank",
+    price: 30,
+    category:"mens",
+    productColor: "Frog Green",
+    productImage: menProduct8,
+
+},
+{
+    id: 209,
+    productName: "Rest Day Track Pants",
+    price: 48,
+    category:"mens",
+    productColor: "Camo Brown",
+    productImage: menProduct9,
+
+},
+{
+    id: 300,
+    productName: "Sharkhead Backpack",
+    productColor:"Black",
+    category:"accessories",
+    price: 36,
+    productImage: accProduct1,
+
+},
+{
+    id: 301,
+    productName: "Nike Air Force 1",
+    productColor:"1 Color",
+    category:"accessories",
+    price: 125,
+    productImage: shoe1,
+
+},
+{
+    id: 302,
+    productName: "Nike Dunk Low",
+    productColor:"1 Color",
+    price: 125,
+    productImage: shoe2,
+
+},
+{
+    id: 303,
+    productName: "Nike V2K Run",
+    productColor:"1 Color",
+    category:"accessories",
+    price: 110,
+    productImage: shoe3,
+
+},
+{
+    id: 304,
+    productName: "Nike InfinityRN 4",
+    productColor:"13 Colors",
+    price: 160,
+    category:"accessories",
+    productImage: shoe4,
+
+},
+{
+    id: 305,
+    productName: "Air Jordan 1 Retro High OG",
+    price: 180,
+    productColor:"1 Color",
+    category:"accessories",
+    productImage: shoe5,
+
+},
+{
+    id: 306,
+    productName: "Nike Interact Run",
+    price: 85,
+    productColor:"3 Colors",
+    category:"accessories",
+    productImage: shoe6,
+
+},
+{
+    id: 307,
+    productName: "Nike Tech Hera",
+    price: 110,
+    productColor:"7 Colors",
+    category:"accessories",
+    productImage: shoe7,
+
+},
+{
+    id: 308,
+    productName: "Nike Calm",
+    price: 50,
+    productColor:"5 Colors",
+    category:"accessories",
+
+    productImage: shoe8,
+
+},
+];
+export default all_product;
